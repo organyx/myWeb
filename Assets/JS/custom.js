26ffd6d4801e36d9d21bcf7b7ce444606557799e
Aleks <aleksandr.beliun@gmail.com>
2015-02-09 15:16:14 +0200

    linked pages

--- Assets/JS/custom.js
+++ Assets/JS/custom.js
@@ -12,4 +12,34 @@
           }, 600);
           return false;
       });
-  });
+/*
+    function isElementInViewport(elem) {
+      var $elem = $(elem);
+
+      var scrollElem = ((navigator.userAgent.toLowerCase().indexOf('webkit') != -1) ? 'body' : 'html');
+      var viewportTop = $(scrollElem).scrollTop();
+      var viewportBottom = viewportTop + $(window).height();
+
+      var elemTop = Math.round( $elem.offset().top );
+      var elemBottom = elemTop + $elem.height();
+
+      return ((elemTop < viewportBottom) && (elemBottom > viewportTop));
+  }
+
+    function checkAnimation() {
+      $('.bar').each(function(){
+        var $e = $(this).find('.fill');
+        
+        if(!$e.hasClass('started')){
+          if (isElementInViewport($e)) {
+            var level = $e.data('level');
+            $e.animate({
+              width: level
+            }, 2000);
+            $e.addClass('started');
+          }
+        }
+      });
+    }*/
+});
+
