9b1bedbd894bade7d53e6394498d4026e4479c3b
Aleks <aleksandr.beliun@gmail.com>
2015-02-09 16:26:38 +0200

    progress bars

--- Assets/JS/custom.js
+++ Assets/JS/custom.js
@@ -12,6 +12,13 @@
           }, 600);
           return false;
       });
+
+jQuery('.skillbar').each(function(){
+  jQuery(this).find('.skillbar-bar').animate({
+    width:jQuery(this).attr('data-percent')
+  },2000);
+});
+
 /*
     function isElementInViewport(elem) {
       var $elem = $(elem);
@@ -42,4 +49,5 @@
       });
     }*/
 });
+
 
