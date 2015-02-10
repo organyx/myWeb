23342c4a7679b30e28b421c49685cc897b3e1a99
Aleks <aleksandr.beliun@gmail.com>
2015-02-10 11:33:28 +0200

    index page

--- Assets/JS/custom.js
+++ Assets/JS/custom.js
@@ -13,41 +13,13 @@
           return false;
       });
 
-jQuery('.skillbar').each(function(){
-  jQuery(this).find('.skillbar-bar').animate({
-    width:jQuery(this).attr('data-percent')
-  },2000);
-});
+  jQuery('.skillbar').each(function(){
+    jQuery(this).find('.skillbar-bar').animate({
+      width:jQuery(this).attr('data-percent')
+    },2000);
+  });
 
-/*
-    function isElementInViewport(elem) {
-      var $elem = $(elem);
 
-      var scrollElem = ((navigator.userAgent.toLowerCase().indexOf('webkit') != -1) ? 'body' : 'html');
-      var viewportTop = $(scrollElem).scrollTop();
-      var viewportBottom = viewportTop + $(window).height();
-
-      var elemTop = Math.round( $elem.offset().top );
-      var elemBottom = elemTop + $elem.height();
-
-      return ((elemTop < viewportBottom) && (elemBottom > viewportTop));
-  }
-
-    function checkAnimation() {
-      $('.bar').each(function(){
-        var $e = $(this).find('.fill');
-        
-        if(!$e.hasClass('started')){
-          if (isElementInViewport($e)) {
-            var level = $e.data('level');
-            $e.animate({
-              width: level
-            }, 2000);
-            $e.addClass('started');
-          }
-        }
-      });
-    }*/
 });
 
 
