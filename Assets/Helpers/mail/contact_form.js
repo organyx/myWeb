676f41539c588efd5563a806ed80ab580e04c0e5
Aleks <aleksandr.beliun@gmail.com>
2015-02-13 11:25:15 +0200

    mail fix sorta

--- Assets/Helpers/mail/contact_form.js
+++ Assets/Helpers/mail/contact_form.js
@@ -1,18 +1,18 @@
-$(document).ready(function(){  
+$(document).ready(function(){
 $("#submit").click(function(){
 	var name = $("#name").val();
 	var email = $("#email").val();
 	var message = $("#message").val();
 
 	$("#returnmessage").empty(); //To empty previous error/success message.
-//checking for blank fields	
+//checking for blank fields
 if(name==''||email=='')
 {
-   alert("Please Fill Required Fields"); 
+   alert("Please Fill Required Fields");
 }
 else{
 // Returns successful data submission message when the entered information is stored in database.
-$.post("contact_form.php",{ name1: name, email1: email, message1:message},
+$.post("Assets/Helpers/mail/contact_form.php",{ name1: name, email1: email, message1:message},
    function(data) {
                 $("#returnmessage").append(data);//Append returned message to message paragraph
 					if(data=="Your Query has been received, We will contact you soon."){
@@ -20,6 +20,6 @@ $.post("contact_form.php",{ name1: name, email1: email, message1:message},
 					}
 			});
          }
- 
+
 });
 });
