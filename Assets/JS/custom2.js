4479b865c429cc5044fb7f5aecbec86a6ee1ffba
Aleks <aleksandr.beliun@gmail.com>
2015-02-19 09:24:51 +0200

    cleaning script

--- Assets/JS/custom2.js
+++ Assets/JS/custom2.js
@@ -102,69 +102,7 @@ $(document).ready(function() {
          }
        });
      });
-/*
-     $(window).on('load resize orientationchange orientationChanged', function(event) {
-    // Stick the #nav to the top of the window
-    var nav = $('#NavBar');
-    var navHomeY = nav.offset().top;
-    var isFixed = false;
-    var $w = $(window);
-    $w.scroll(function() {
-      if($(window).width() >= 820) {
-        var scrollTop = $w.scrollTop();
-        var shouldBeFixed = scrollTop > navHomeY;
-        if (shouldBeFixed && !isFixed) {
-            nav.css({
-                position: 'fixed',
-                top: 0,
-                left: nav.offset().left,
-                width: nav.width()
-            });
-            isFixed = true;
-        }
-        else if (!shouldBeFixed && isFixed)
-        {
-            nav.css({
-                position: 'static'
-            });
-            isFixed = false;
-        }
-      }
-      else {
 
-      }
-    });
-});*/
-     /*
-    $(function() {
-      // only on descktop
-        if($(window).width() >= 820) {
-        // grab the initial top offset of the navigation
-        var sticky_navigation_offset_top = $('#NavBar').offset()
-            .top;
-        // our function that decides weather the navigation bar should have "fixed" css position or not.
-        var sticky_navigation = function() {
-            var scroll_top = $(window).scrollTop(); // our current vertical position from the top
-            // if we've scrolled more than the navigation, change its position to fixed to stick to top, otherwise change it back to relative
-            if (scroll_top > sticky_navigation_offset_top) {
-                $('#NavBar').css({
-                    'position': 'fixed',
-                    'top': 0
-                });
-            } else {
-                $('#NavBar').css({
-                    'position': 'relative'
-                });
-            }
-        };
-        // run our function on load
-        sticky_navigation();
-        // and run it again every time you scroll
-        $(window).scroll(function() {
-            sticky_navigation();
-        });
-      }
-    });*/
     /**************
         Skill bars
      */
